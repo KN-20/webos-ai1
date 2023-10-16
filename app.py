4c3807a6e167982b5a9f6f59c01c8b8afcf626f0
KN-20 <2020112323@student.kyungnam.ac.kr>
2023-10-16 14:14:24 +0900

    KN-20
    수정

--- app.py
+++ app.py
@@ -1,49 +1,40 @@
 from flask import Flask, request, jsonify
-from PIL import Image
-from io import BytesIO
-from ultralytics import YOLO
-import cv2
-import time
-import base64
-import numpy as np
 
-global model
-model = YOLO('hbest.pt')
+import Img_Proc as ip
 
 app = Flask(__name__)
-
-@app.route('/timeout', methods=['POST'])
-def timeout():
-    time.sleep(120)
-    return 0
+@app.route('/ImgRecive', methods=['GET', 'POST'])
+def send_image():
+    data = ip.send_food_data()
+    return data
 
 @app.route('/ImgSend', methods=['POST'])
 def receive_image():
     try:
         data = request.get_json()
-        image_data = data.get('data')
-        img = save_image(image_data)
-        if len(img) < 1:
-            return jsonify({'status': 'error'})
-        image = yolo_image(img)
-        return jsonify({'status': 'success', 'food': '마가렛트', 'allergy':['우유', '밀'], 'date':'2024-01-01', 'data': str(image)})
-    except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)})
-
-def save_image(encoded_image):
-    try:
         print(1)
-        image_data = BytesIO(bytes(encoded_image, 'utf-8'))
+        image_data = data.get('data')
         print(2)
-        image = Image.open(BytesIO(base64.b64decode(encoded_image)))
-        image = np.array(image)
-        return image
-    except Exception as e:
-        print(f"Error saving image: {e}")
-        return
+        img, img_path = ip.save_image(image_data)
+        print(img)
+        if img == 'no_data':
+            print(len(img))
+            return 'error'
+        print(4)
+        print(img_path)
+        fo_na = ip.barcode_decode(img_path)
 
-def yolo_image(image):
-    res = model.predict(source=image)
-    return res
+        print(5)
+        print(img_path)
+        allergy_date = ip.read_text(img_path)
+        print(6)
+        ip.food_save(fo_na, allergy_date[0], allergy_date[1])
+        print(7)
+        return 'success'
+    except Exception as e:
+        print(e)
+        return 'error'
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5501)
+    app.run(host='0.0.0.0', port=5501,debug=True)
